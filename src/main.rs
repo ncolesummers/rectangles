507332812c7815d4741b1893228441db861f7f02
Cole Summers <nsummers72@gmail.com>
2021-03-09 00:04:33 -0800

    done with structs

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,28 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!(
-        "rect1 is {:?} ",
-        (rect1)
-        );
-}
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
 
-//fn area(rectangle: &Rectangle) -> u32 {
-  //  rectangle.width * rectangle.height
-//}
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!( "Can rect1 hold rect2? {}.", rect1.can_hold(&rect2));
+    println!( "Can rect1 hold rect3? {}.", rect1.can_hold(&rect3));
+}
